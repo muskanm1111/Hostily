747ccb413da6bf4d56c6829a89b6cf47401f29dc
muskan manjhi <muskanmanjhi15@gmail.com>
2025-03-31 18:18:10 +0530

    Update font size in Offer component for improved readability

--- src/components/offer.jsx
+++ src/components/offer.jsx
@@ -8,10 +8,10 @@ const Offer = () => {
         <div className="absolute inset-0 bg-black bg-opacity-70 flex items-center justify-start z-10">
           <div className="text-center   mx-auto  max-w-7xl text-white  ">
             <div className="flex flex-col items-center md:items-center justify-center px-4  ">
-              <span className="text-[2rem] capitalize md:text-6xl     font-semibold mb-0 md:mb-4">
+              <span className="text-3xl capitalize md:text-6xl     font-semibold mb-0 md:mb-4">
                 Book hotel rooms get
               </span>
-              <span className="text-[2rem] capitalize md:text-6xl   font-semibold ">
+              <span className="text-3xl capitalize md:text-6xl   font-semibold ">
                 deals & book flights online.
               </span>
             </div>
